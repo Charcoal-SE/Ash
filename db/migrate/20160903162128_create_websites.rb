b956187a121fc915bc5751ac9fb1051557f9623b
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-09-03 18:53:09 +0100

    fix migrations & auth

--- db/migrate/20160903162128_create_websites.rb
+++ db/migrate/20160903162128_create_websites.rb
@@ -2,7 +2,7 @@ class CreateWebsites < ActiveRecord::Migration[5.0]
   def change
     create_table :websites do |t|
       t.string :url
-      t.int :post_count
+      t.integer :post_count
 
       t.timestamps
     end
