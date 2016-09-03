2cd4933485163b4bc7530a4358af6ad044b9e76f
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-09-03 19:46:45 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root :to => 'dashboard#index'
+
+  get    'websites',                                         :to => 'websites#index'
+  get    'websites/new',                                     :to => 'websites#new'
+  post   'websites/new',                                     :to => 'websites#create'
+  get    'websites/:id',                                     :to => 'websites#show'
+  get    'websites/:id/edit',                                :to => 'websites#edit'
+  patch  'websites/:id/edit',                                :to => 'websites#update'
+  delete 'websites/:id',                                     :to => 'websites#destroy'
 end
