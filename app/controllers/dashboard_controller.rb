475601a0c2c6a71ccd9262f8142ca90f34f6118c
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-09-04 13:15:40 +0100

    admin partial

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class DashboardController < ApplicationController
   def index
     @websites = Website.order(:created_at => :desc).limit(5)
+    @admins = Admin.order(:site_count => :desc).limit(5)
   end
 end
