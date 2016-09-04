dde9e13b9efe37e13612791e53f938942aef27de
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-09-04 13:29:33 +0100

    full dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,7 @@ class DashboardController < ApplicationController
   def index
     @websites = Website.order(:created_at => :desc).limit(5)
     @admins = Admin.order(:site_count => :desc).limit(5)
+    @techs = Tech.order(:site_count => :desc).limit(5)
+    @registrars = Registrar.order(:site_count => :desc).limit(5)
   end
 end
