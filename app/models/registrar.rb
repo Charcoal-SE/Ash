79a0dae31b0afa9ac29e8200ac52fe863ff89b3d
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-09-05 12:41:15 +0100

    URL validation

--- app/models/registrar.rb
+++ app/models/registrar.rb
@@ -2,4 +2,12 @@ class Registrar < ApplicationRecord
   has_many :websites
 
   validates_presence_of :name
+  validate :valid_url
+
+  private
+    def valid_url
+      if !URI.parse(url)
+        errors.add(:url, "doesn't appear to be a valid URL")
+      end
+    end
 end
--- app/models/website.rb
+++ app/models/website.rb
@@ -2,4 +2,13 @@ class Website < ApplicationRecord
   belongs_to :registrar
   belongs_to :tech
   belongs_to :admin
+
+  validate :valid_url
+
+  private
+    def valid_url
+      if !URI.parse(url)
+        errors.add(:url, "doesn't appear to be a valid URL")
+      end
+    end
 end
