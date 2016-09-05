2005758b73e2702ebf2eecb75ef717c3d2aa0f38
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-09-05 16:15:59 +0100

    basic methods

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,2 +1,39 @@
 class ApiController < ApplicationController
+  before_action :verify_key
+
+  # Write methods
+
+  def create_website
+  end
+
+  def create_admin
+  end
+
+  def create_tech
+  end
+
+  def create_registrar
+  end
+
+  # Read methods
+
+  def websites
+  end
+
+  def admins
+  end
+
+  def techs
+  end
+
+  def registrars
+  end
+
+  private
+    def verify_key
+      @key = ApiKey.find_by_key params[:key]
+      unless params[:key].present? && @key.present?
+        render :status => 403, :json => { :error_code => 403, :error_name => 'invalid_key', :error_message => 'No key was passed or the passed key is invalid.' } and return
+      end
+    end
 end
